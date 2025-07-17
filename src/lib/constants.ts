ea21b415053ea7994a594f4fb68597fe7492f0e6
Ronaldo Ryan <ronaldoryanrrs@gmail.com>
2025-07-16 18:57:46 -0600

    arreglo de API_KEY

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 // English: Base URL for the API and the API key for authentication.
 // Español: URL base para la API y la clave de API para autenticación.
-export const API_BASE_URL = "http://localhost:3001/api";
-export const API_KEY = "4TRCdQruboPb7lB2dpGbn8nJpVDZyykLKCy0eGjD";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL!;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY!;
+
